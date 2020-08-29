54bb95096a92bf77d1c9662b0c18f2b164886868
Siddharth <esiddharth1894@gmail.com>
2020-08-29 14:18:59 +0530

    Implemented Two Pass Sort Algorithm

---
+++ Arrays/src/arrays/DutchNationalFlag.java
@@ -0,0 +1,47 @@
+package arrays;
+
+/*
+* Given an array A of elements (0,1,2) and a pivot index i into A, Rearrange the elements such that
+* all elements less than pivot appears first, followed by elements equal to the pivot,
+* followed by elements greater than the pivot
+*
+* Constraints:
+*   A.length >= 3
+*   A[i] => {0,1,2}
+* */
+public class DutchNationalFlag {
+
+    public void sort(int[] array, int pivot) {
+        twoPassSort(array);
+    }
+
+    private void twoPassSort(int[] array) {
+        // Left to Right
+        int i = -1;
+        int j = 0;
+
+        while(j < array.length) {
+            if(array[j] == 0) {
+                swap(array, ++i, j);
+            }
+            j ++;
+        }
+
+        // Right to Left
+        i = array.length;
+        j = array.length-1;
+
+        while(j >= 0) {
+            if(array[j] == 2) {
+                swap(array, --i, j);
+            }
+            j --;
+        }
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
